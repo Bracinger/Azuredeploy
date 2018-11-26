7ef7f4443e54447bb91c55d18a72d8c4ffca92df
Ruslan Kyrnytskyi <Ruslan.Kyrnytskyi@teaminternational.com>
2018-11-26 10:50:02 +0200

    add scripts to rep

---
+++ testcompare.ps1
@@ -0,0 +1,18 @@
+﻿$folder1 = "D:\test1"
+$folder2 = "D:\test2"
+
+# Get all files under $folder1, filter out directories
+$firstFolder = Get-ChildItem -Recurse $folder1 
+
+$firstFolder | ForEach-Object {
+
+    # Check if the file, from $folder1, exists with the same path under $folder2
+    
+        # Compare the contents of the two files...
+        if(Compare-Object $folder1 $folder2) {
+
+            # List the paths of the files containing diffs
+            $_.FullName
+            $_.FullName.Replace($folder1, $folder2)
+       } 
+}
