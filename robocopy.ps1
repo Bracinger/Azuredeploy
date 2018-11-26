750f04804a90bec084fcf94a1ab51cf3db338a0b
Руслан Кирницкий <bracinger@gmail.com>
2018-11-26 15:39:22 +0200

    robocopy changes

--- robocopy.ps1
+++ robocopy.ps1
@@ -1,1 +1,4 @@
-﻿robocopy C:\tfold D:\tfold /MIR /E /V /xf conf.config /xd AppData
+﻿$1 = Get-Item C:\erecruit\erecruitservices
+$2 = Get-Item C:\erecruit\Buslistener
+$3 = Get-Item C:\erecruit\srch1
+robocopy C:\erecruit D:\erecruitservices /MIR /PURGE /E /V /xf *.config *.odt *.pdf /xd AppData $3 $2
--- testcompare.ps1
+++ testcompare.ps1
@@ -1,18 +1,9 @@
-﻿$folder1 = "D:\test1"
-$folder2 = "D:\test2"
-
-# Get all files under $folder1, filter out directories
-$firstFolder = Get-ChildItem -Recurse $folder1 
-
-$firstFolder | ForEach-Object {
-
-    # Check if the file, from $folder1, exists with the same path under $folder2
-    
-        # Compare the contents of the two files...
-        if(Compare-Object $folder1 $folder2) {
-
-            # List the paths of the files containing diffs
-            $_.FullName
-            $_.FullName.Replace($folder1, $folder2)
-       } 
+﻿$pushcontent = Get-ChildItem C:\erecruit -Recurse 
+If ($erefolder = Get-ChildItem -Recurse | Where-Object { $_.DirectoryName -match 'erecruitservices' }) {
+	robocopy $pushcontent $erefolder /MIR /E /V /xf *.config /xd AppData
 }
+
+elseif ($Busfolder = Get-ChildItem -Path D:\Buslistener) {
+	robocopy $pushcontent $Busfolder /MIR /E /V /xf *.config /xd AppData
+}
+
